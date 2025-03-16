605259e4882298fd8763f58519ce706a9a501697
kovarmi9 <kovarmi9@seznam.cz>
2025-03-16 22:29:22 +0100

    Set up the application icon

--- U1/src/main.cpp
+++ U1/src/main.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // Set up the application icon
+    a.setWindowIcon(QIcon(":/icons/pointpol.png"));
     MainForm w;
     w.show();
     return a.exec();
